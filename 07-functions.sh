2e772aa35b6f59bd408ac9429aa2c21feea822ce
kumar <kumardhayait15@gmail.com>
2022-12-23 16:15:39 +0530

    Override

--- 07-functions.sh
+++ 07-functions.sh
@@ -1,6 +1,11 @@
 ## declaration way 1
  function SAMPLE() {
+     echo a from Main Program = $a
+       a=100
+       echo  a in  FUNCTION after override in function - $a
      echo  SAMPLE FUNCTION
+     b=10
+     echo  first Arguments to script = $1
  }
 
 
@@ -13,5 +18,10 @@ SAMPLE1() {
   echo SAMPLE1 FUNCTION
 }
 
+## Main Program
+a=10
 SAMPLE
 SAMPLE1
+echo b from FUNCTION = $b
+b=200
+echo  b from function after override in Main program - $b
