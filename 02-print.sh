c63d9358001d2053d39d2ee2b0427472d04c8ca8
kumar <kumardhayait15@gmail.com>
2022-12-23 15:45:12 +0530

    color checking

--- 02-print.sh
+++ 02-print.sh
@@ -1,1 +1,19 @@
 echo Hello world
+
+## Color Print Syntax
+
+#echo -e "\e[COLmMESSAGE\e[0m"
+# Ex : echo -e "e[31mHELLO\e[0m"
+# \e[31m -> to enable color code 31
+# \e[0m --> To disabled the enabled color we use 0
+# -e  --> Enable esc se1. \e is one esc seq
+# ""  --> Quotes are mandatory if we use esc s
+
+### colors
+
+# RED      31
+# GREEN    32
+# YELLOW   33
+# BLUE     34
+# MAGENTA  35
+# CYAN     36
